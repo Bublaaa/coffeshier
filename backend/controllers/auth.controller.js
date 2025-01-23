895dabd27f33582ec9b65923f66ceeffe62a6324
Bublaaa <yanuarkrisna7@gmail.com>
2025-01-23 20:34:24 +0700

    Add Send Verification Email

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,74 @@
+import { User } from '../models/User.js'
+import bcrypt from 'bcryptjs'
+import generateTokenAndSetCookie from '../utils/generateTokenAndSetCookie.js'
+import { sendVerificationEmail } from '../mailtrap/email.js'
+
 export const signup = async (req,res) => {
-    res.send("signup route")
+    const{email, password, name, position } = req.body
+    try {
+        if(!email || !password || !name){
+            throw new Error("All fields are required")
+        }
+
+        const userAlreadyExists = await User.findOne({email})
+        if(userAlreadyExists) {
+            return res.status(400).json({success:false, message:"User already exist"})
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString()
+
+        const user = new User({
+            email,
+            password: hashedPassword,
+            name,
+            position,
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000
+        }) 
+
+        generateTokenAndSetCookie(res, user._id)
+        sendVerificationEmail(user.email, verificationToken)
+
+        await user.save()
+
+        res.status(201).json({
+            success: true,
+            message: "User cerated successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            }
+        })
+
+    } catch (error) {
+        res.status(400).json({success:false, message: error.message})
+    }
+}
+
+export const verifyEmail = async (req, res) => {
+    const {code} = req.body
+
+    try {
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationTokenExpiresAt: { $gt: Date.now }
+            // $gt = greater than
+        })
+
+        if(!user){
+            return res.status(400).json({success: false, mesasge: "Invalid or expired verification code"})
+        }
+
+        user.isVerified = true
+        user.verificationToken = undefined
+        user.verificationTokenExpiresAt = undefined
+
+        await user.save()
+
+        await sendWelcomeEmail(user.email, user.name)
+    } catch (error) {
+        
+    }
 }
 
 export const login = async (req,res) => {
--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,14 @@ import authRoutes from './routes/auth.js'
 dotenv.config()
 
 const app = express();
+const port = process.env.PORT || 5001
 
-app.get("/", (req, res) => {
-    res.send("Hello W")
-})
-
+// Allow to parse incoming requests with json "req.body"
+app.use(express.json())
 // Prefix for auth routes "/api/auth/login"
 app.use("/api/auth", authRoutes)
 
-app.listen(3000, () => {
+app.listen(port, () => {
     connection()
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port:", port)
 })
