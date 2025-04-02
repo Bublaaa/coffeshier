c0f467dd1ea5288336e893f704a52e14220e9b1a
Bublaaa <yanuarkrisna7@gmail.com>
2025-04-02 21:14:10 +0700

    Add modal to editable menu data

--- frontend/src/pages/ProductDetailPage.jsx
+++ frontend/src/pages/ProductDetailPage.jsx
@@ -6,9 +6,11 @@ import { useEffect, useReducer, useState } from "react";
 import { motion } from "framer-motion";
 import { NavLink } from "react-router-dom";
 import { placeholder } from "../assets/index.js";
-import { RadioInput, DropdownInput } from "../components/Input";
+import { RadioInput, DropdownInput, Input } from "../components/Input";
 import * as LucideIcons from "lucide-react";
 import Button from "../components/Button";
+import Modal from "../components/Modal.jsx";
+import toast from "react-hot-toast";
 
 const ProductDetailPage = () => {
   const { id } = useParams();
@@ -25,7 +27,6 @@ const ProductDetailPage = () => {
   } = useCategoryStore();
   let selectedQuantity = 0;
   let selectedUnit = "";
-  let selectedAdditionalPrice = 0;
   const initialState = {
     menus: {
       name: "",
@@ -44,7 +45,46 @@ const ProductDetailPage = () => {
       recipe: "",
     },
     selectedSize: "regular",
+    selectedAdditionalPrice: 0,
+    totalPrice: 0,
     errors: {},
+    isModalOpen: false,
+    modalBody: null,
+    modalTitle: "",
+    modalSize: "small",
+  };
+
+  const EditStatusForm = ({}) => {
+    return (
+      <form>
+        <DropdownInput
+          label="Status"
+          name="status"
+          value={state.menus.status}
+          options={[
+            { value: "Available", label: "Available" },
+            { value: "Not Available", label: "Not Available" },
+          ]}
+          onChange={handleChangeStatus}
+        />
+      </form>
+    );
+  };
+  const EditCategoryForm = ({ categories }) => {
+    return (
+      <form>
+        <DropdownInput
+          label="Category"
+          name="categoryId"
+          value={state.menus.categoryId}
+          options={categories.map((category) => ({
+            value: category._id,
+            label: category.name,
+          }))}
+          onChange={handleCategoryChange}
+        />
+      </form>
+    );
   };
 
   const menuReducer = (state, action) => {
@@ -57,6 +97,23 @@ const ProductDetailPage = () => {
         return { ...state, status: action.payload };
       case "SET_SELECTED_SIZE":
         return { ...state, selectedSize: action.payload };
+      case "SET_SELECTED_UNIT":
+        return { ...state, selectedSize: action.payload };
+      case "SET_SELECTED_QUANTITY":
+        return { ...state, selectedQuantity: action.payload };
+      case "SET_SELECTED_ADDITIONAL_PRICE":
+        return { ...state, selectedAdditionalPrice: action.payload };
+      case "SET_TOTAL_PRICE":
+        return { ...state, totalPrice: action.payload };
+
+      case "SET_MODAL_OPEN":
+        return { ...state, isModalOpen: action.payload };
+      case "SET_MODAL_BODY":
+        return { ...state, modalBody: action.payload };
+      case "SET_MODAL_TITLE":
+        return { ...state, modalTitle: action.payload };
+      case "SET_MODAL_SIZE":
+        return { ...state, modalSize: action.payload };
 
       case "SET_INGREDIENTS":
         return { ...state, ingredients: action.payload };
@@ -78,7 +135,21 @@ const ProductDetailPage = () => {
   const handleSizeChange = (e) => {
     const { value } = e.target;
     dispatch({ type: "SET_SELECTED_SIZE", payload: value });
+
+    let additionalPrice = 0;
+    menus.sizes.forEach((menu) => {
+      if (menu.size === value) {
+        additionalPrice = menu.additionalPrice;
+        dispatch({
+          type: "SET_SELECTED_ADDITIONAL_PRICE",
+          payload: additionalPrice,
+        });
+      }
+    });
+    const totalPrice = state.menus.basePrice + additionalPrice;
+    dispatch({ type: "SET_TOTAL_PRICE", payload: totalPrice });
   };
+
   const handleCategoryChange = (e) => {
     const { value } = e.target;
     dispatch({ type: "SET_CATEGORY", payload: value });
@@ -88,8 +159,41 @@ const ProductDetailPage = () => {
     dispatch({ type: "SET_STATUS", payload: value });
   };
 
+  const handleOpenModal = (title, body, size) => {
+    dispatch({ type: "SET_MODAL_OPEN", payload: true });
+    dispatch({ type: "SET_MODAL_TITLE", payload: title });
+    dispatch({ type: "SET_MODAL_BODY", payload: body });
+    dispatch({ type: "SET_MODAL_SIZE", payload: size });
+  };
+
+  const handleMenuChange = (e) => {
+    const target = e.target.closest("[data-id]");
+    if (!target) return;
+    switch (target.dataset.id) {
+      case "status":
+        handleOpenModal("Change Menu Name", <EditStatusForm />);
+        break;
+      case "category":
+        handleOpenModal(
+          "Change Category Name",
+          <EditCategoryForm categories={categories} />
+        );
+        break;
+
+      default:
+        toast.error("Unknown menu change request:" + target.dataset.id);
+    }
+  };
+
   return (
     <div className=" md:gap-5 gap-2 md:my-5 my-2 md:mr-5 mr-2 transition-all ease-in-out duration-300 h-[95vh]">
+      <Modal
+        isOpen={state.isModalOpen}
+        title={state.modalTitle}
+        body={state.modalBody}
+        onClose={() => dispatch({ type: "SET_MODAL_OPEN", payload: false })}
+      />
+
       <div className="flex flex-col md:gap-5 gap-2 h-full">
         <div className="flex justify-between items-center p-1">
           <Button buttonSize="icon" buttonType="secondary">
@@ -99,48 +203,85 @@ const ProductDetailPage = () => {
           </Button>
         </div>
         <div className="absolute inset-x-100 inset-y-10 flex items-center justify-end w-1/2 z-5">
-          <img
+          <motion.img
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.5, ease: "easeInOut" },
+            }}
             src={state.menus.image || placeholder}
             alt={state.menus.name || "Placeholder"}
-            className="max-w-full max-h-full object-contain"
+            className="max-w-full max-h-full object-contain hover:scale-105 hover:rotate-5"
           />
         </div>
-        <div className="relative z-10 flex flex-row md:gap-5 gap-2 h-full">
+        <div className="relative z-10 flex flex-row md:gap-5 gap-2 h-fit mt-auto">
           <motion.div
+            onClick={(e) => handleMenuChange(e)}
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             className="flex flex-col w-1/3 p-5 rounded-lg gap-5 bg-white mt-auto"
           >
-            <h1 className="line-clamp-2 max-w-sm whitespace-normal">
+            <div className="flex flex-row gap-2 md:gap-5 items-center">
+              {/* Status Display */}
+              <div
+                data-id="status"
+                className={`px-3 py-2 rounded-lg font-semibold hover:cursor-pointer hover:scale-105 ${
+                  state.menus.status === "Not Available"
+                    ? "bg-red-200 text-red-800"
+                    : "bg-green-200 text-green-300"
+                }`}
+              >
+                <p
+                  className={`${
+                    state.menus.status === "Not Available"
+                      ? "text-red-700"
+                      : "text-green-700"
+                  }`}
+                >
+                  {state.menus.status}
+                </p>
+              </div>
+              {/* Category Display */}
+              {categories
+                .filter((category) => category._id === state.menus.categoryId)
+                .map((category) => {
+                  const IconComponent = category.icon
+                    ? LucideIcons[category.icon]
+                    : LucideIcons.HandPlatter;
+                  return (
+                    <div
+                      key={category._id}
+                      data-id="category"
+                      className="flex flex-row items-center gap-2 px-3 py-2 bg-accent rounded-lg hover:cursor-pointer hover:scale-105 hover:bg-accent-hover"
+                    >
+                      <IconComponent className="size-5 text-white" />
+                      <p className="font-semibold text-white">
+                        {category.name.replace(/\b\w/g, (char) =>
+                          char.toUpperCase()
+                        )}
+                      </p>
+                    </div>
+                  );
+                })}
+            </div>
+            {/* Menu Name */}
+            <h1 className="line-clamp-3 max-w-sm whitespace-normal hover:cursor-pointer hover:scale-101 hover:bg-gray-100 rounded-lg px-2">
               {menus.name}
             </h1>
-            <p>{menus.descriptions || "No description available "}</p>
-            <div className="flex flex-row gap-2 items-center">
-              <DropdownInput
-                label="Status"
-                name="status"
-                value={state.menus.status}
-                options={[
-                  { value: "Available", label: "Available" },
-                  { value: "Not Available", label: "Not Available" },
-                ]}
-                onChange={handleChangeStatus}
-              />
-              <DropdownInput
-                label="Category"
-                name="categoryId"
-                value={state.menus.categoryId}
-                options={categories.map((category) => ({
-                  value: category._id,
-                  label: category.name,
-                }))}
-                onChange={handleCategoryChange}
-              />
+            {/* Menu Description */}
+            <p className="hover:cursor-pointer hover:scale-101 hover:bg-gray-100 rounded-lg p-2">
+              {menus.descriptions || "No description available "}
+            </p>
+            {/* Menu Recipe */}
+            <div className="gap-2 hover:cursor-pointer hover:scale-101 hover:bg-gray-100 rounded-lg p-2">
+              <h6>Recipe</h6>
+              <p>{state.menus.recipe || "No recipe available "}</p>
             </div>
-            <p>{state.menus.recipe || "No recipe available "}</p>
           </motion.div>
+          {/* Price */}
           <div className="flex flex-col gap-2 p-2 w-1/3 justify-end items-end">
-            <div className="flex flex-row w-full justify-between items-center text-dark">
+            <div className="flex flex-row w-full justify-between items-center text-dark hover:cursor-pointer hover:scale-101 hover:bg-gray-200 rounded-lg hover:p-2">
               <h6>Base Price</h6>
               <p>
                 {state.menus.basePrice
@@ -148,16 +289,18 @@ const ProductDetailPage = () => {
                   : "0"}
               </p>
             </div>
-            <div className="flex flex-row w-full justify-between items-center border-b border-gray-300 pb-3">
-              <h6>Additional</h6>
-              <p>
-                {selectedAdditionalPrice >= 0
-                  ? selectedAdditionalPrice.toLocaleString("id-ID")
-                  : "0"}
-              </p>
+            {state.selectedAdditionalPrice > 0 && (
+              <div className="flex flex-row w-full justify-between items-center">
+                <h6>Size Price</h6>
+                <p>{state.selectedAdditionalPrice.toLocaleString("id-ID")}</p>
+              </div>
+            )}
+            <div className="flex flex-row w-full justify-between items-center border-t border-gray-300">
+              <h5>Total Price</h5>
+              <h6>{state.totalPrice.toLocaleString("id-ID")}</h6>
             </div>
           </div>
-          <motion.div className="w-1/3 mt-auto h-fit p-5 bg-white rounded-lg ">
+          <motion.div className="w-1/3 flex flex-col gap-2 mt-auto h-fit p-5 bg-white rounded-lg ">
             <RadioInput
               options={
                 menus?.sizes?.map((item) => ({
@@ -182,27 +325,31 @@ const ProductDetailPage = () => {
                   if (state.selectedSize == size.size) {
                     selectedQuantity = size.quantity;
                     selectedUnit = size.unit;
-                    selectedAdditionalPrice = size.additionalPrice;
                   }
                 });
                 return selectedIngredient ? (
                   <div
-                    className="grid grid-cols-3 border-b border-gray-100 py-2 items-center"
-                    key={selectedIngredient._id || selectedIngredient.name}
+                    className="flex flex-col gap-1"
+                    key={selectedIngredient._id}
                   >
-                    <p>
-                      {selectedIngredient.name.replace(/\b\w/g, (char) =>
-                        char.toUpperCase()
-                      )}
-                    </p>
-                    <p className="text-center">
-                      {selectedQuantity !== null
-                        ? selectedQuantity
-                        : "No quantity found"}
-                    </p>
-                    <p>
-                      {selectedUnit !== null ? selectedUnit : "No Unit found"}
-                    </p>
+                    <div
+                      className="flex flex-row hover:cursor-pointer hover:scale-101 hover:bg-gray-200 rounded-lg p-2"
+                      key={selectedIngredient._id || selectedIngredient.name}
+                    >
+                      <p>
+                        {selectedIngredient.name.replace(/\b\w/g, (char) =>
+                          char.toUpperCase()
+                        )}
+                      </p>
+                      <p className="text-center ml-auto">
+                        {selectedQuantity !== null
+                          ? selectedQuantity
+                          : "No quantity found"}
+                      </p>
+                      <p>
+                        {selectedUnit !== null ? selectedUnit : "No Unit found"}
+                      </p>
+                    </div>
                   </div>
                 ) : (
                   <p key={ingredient._id || ingredient.name}>
